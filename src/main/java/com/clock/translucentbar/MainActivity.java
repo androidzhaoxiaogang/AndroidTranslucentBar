8bd60b905fc61e04933ad9be6b655957258fbd1d
clock <1084991652clock@gmail.com>
2015-07-24 17:32:19 +0800

    完成提交工程

---
+++ src/main/java/com/clock/translucentbar/MainActivity.java
@@ -0,0 +1,21 @@
+package com.clock.translucentbar;
+
+import android.app.Activity;
+import android.os.Build;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
---
+++ src/main/java/com/clock/translucentbar/TranslucentApplication.java
@@ -0,0 +1,14 @@
+package com.clock.translucentbar;
+
+import android.app.Application;
+
+/**
+ * Created by Administrator on 2015/7/23.
+ */
+public class TranslucentApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
